ebe88ab41c8c9a4095b2fda927d99b1fdd8d99b0
mirimhee <48482259+LIMMIHEE@users.noreply.github.com>
2023-11-07 21:15:56 +0900

    [chore] 프로젝트 구조 구축

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:cow_cold/firebase_options.dart';
 import 'package:cow_cold/view/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: 'assets/.env');
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
