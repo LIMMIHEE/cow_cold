8c9d34a396f88c56f823a7ff7516386987bd94ca
mirimhee <48482259+LIMMIHEE@users.noreply.github.com>
2023-12-18 23:24:57 +0900

    feat : 감상 바텀 시트 full width 적용

--- lib/view/widget/work_detail/work_detail_report_bottom_sheet.dart
+++ lib/view/widget/work_detail/work_detail_report_bottom_sheet.dart
@@ -20,90 +20,93 @@ class WorkDetailReportBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(
-                      height: 24,
-                    ),
-                    Text(
-                      DateFormat.yMMMd('ko').format(DateTime.now()),
-                      style: DesignSystem.typography.body2(TextStyle(
-                          color: DesignSystem.colors.gray700,
-                          fontWeight: FontWeight.w400)),
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    SingleChildScrollView(
-                      child: Text(
-                        report.content,
-                        style: DesignSystem.typography.body(),
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        height: 24,
                       ),
-                    ),
-                  ],
-                )),
-          ),
-          const SizedBox(
-            height: 12,
-          ),
-          Visibility(
-            visible: isMyReport,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 12),
-              child: Row(
-                children: [
-                  Expanded(
-                      child: BottomButton(
-                          text: '감상 삭제',
-                          backgroundColor: DesignSystem.colors.deleteRed,
-                          textColor: DesignSystem.colors.white,
-                          onTap: () {
-                            Get.back();
-                            Get.dialog(CommonDialog(
-                              title: '감상 삭제하기',
-                              subText: '정말 감상을 삭제하시겠습니까?\n삭제 후 복구는 불가능합니다.',
-                              confirmAction: () {
-                                Get.back();
-                                Future.delayed(const Duration(milliseconds: 50),
-                                    () {
-                                  Get.find<ReportController>()
-                                      .deleteReport(report);
-                                });
-                              },
-                            ));
-                          },
-                          paddingVisible: false)),
-                  const SizedBox(
-                    width: 12,
-                  ),
-                  Expanded(
-                      child: BottomButton(
-                          text: '감상 수정',
-                          backgroundColor: DesignSystem.colors.appSecondary,
-                          textColor: DesignSystem.colors.white,
-                          onTap: () {
-                            Get.back();
-                            Get.toNamed(
-                              '/write_report',
-                              arguments: {
-                                "initialWorkId": report.workServerId,
-                                "initialReport": report
-                              },
-                            );
-                          },
-                          paddingVisible: false)),
-                ],
-              ),
+                      Text(
+                        DateFormat.yMMMd('ko').format(DateTime.now()),
+                        style: DesignSystem.typography.body2(TextStyle(
+                            color: DesignSystem.colors.gray700,
+                            fontWeight: FontWeight.w400)),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      SingleChildScrollView(
+                        child: Text(
+                          report.content,
+                          style: DesignSystem.typography.body(),
+                        ),
+                      ),
+                    ],
+                  )),
             ),
-          )
-        ],
+            const SizedBox(
+              height: 12,
+            ),
+            Visibility(
+              visible: isMyReport,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 12),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: BottomButton(
+                            text: '감상 삭제',
+                            backgroundColor: DesignSystem.colors.deleteRed,
+                            textColor: DesignSystem.colors.white,
+                            onTap: () {
+                              Get.back();
+                              Get.dialog(CommonDialog(
+                                title: '감상 삭제하기',
+                                subText: '정말 감상을 삭제하시겠습니까?\n삭제 후 복구는 불가능합니다.',
+                                confirmAction: () {
+                                  Get.back();
+                                  Future.delayed(const Duration(milliseconds: 50),
+                                      () {
+                                    Get.find<ReportController>()
+                                        .deleteReport(report);
+                                  });
+                                },
+                              ));
+                            },
+                            paddingVisible: false)),
+                    const SizedBox(
+                      width: 12,
+                    ),
+                    Expanded(
+                        child: BottomButton(
+                            text: '감상 수정',
+                            backgroundColor: DesignSystem.colors.appSecondary,
+                            textColor: DesignSystem.colors.white,
+                            onTap: () {
+                              Get.back();
+                              Get.toNamed(
+                                '/write_report',
+                                arguments: {
+                                  "initialWorkId": report.workServerId,
+                                  "initialReport": report
+                                },
+                              );
+                            },
+                            paddingVisible: false)),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
