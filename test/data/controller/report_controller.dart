bc9604c3aa9c2bb69d0a094d7035653459086785
mirimhee <48482259+LIMMIHEE@users.noreply.github.com>
2024-01-14 23:07:19 +0900

    test : 테스트 컨트롤러 및 코드 개선

--- test/data/controller/report_controller.dart
+++ test/data/controller/report_controller.dart
@@ -1,9 +1,47 @@
+import 'dart:convert';
+
 import 'package:cow_cold/controllers/report_controller.dart';
+import 'package:cow_cold/data/models/reactions/reaction.dart';
+import 'package:cow_cold/data/models/report/report.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:get/get.dart';
+
+import '../repository/test_repository.dart';
 
 class MockReportController extends ReportController {
+  final fireStore = FakeFirebaseFirestore();
+  late TestRepository testRepository;
 
   @override
   Future<void> onInit() async {
     userId = 'testUserId';
+    testRepository = TestRepository(firestore: fireStore);
+    await loadReports();
+  }
+
+  Future<void> loadReports() async {
+    // 데이터 추가
+    final newReport = const Report(
+      serverId: 'testId',
+      workServerId: 'workServerId',
+      createUserId: 'createUserId',
+      createUserName: 'tester',
+      updateDate: '20240103',
+      title: '제목',
+      content: '내용',
+      reactions: [],
+    ).toJson();
+
+    newReport['reactions'] = [
+      const Reaction(emoji: '❤️', reactionUsers: ['test', 'test2'])
+    ].map((e) => e.toJson()).toList();
+    fireStore.collection('report').add(newReport);
+
+    //데이터 가져오기 및 입력
+    final result = await testRepository.fetchCollection('report');
+    List<Report> reports = result.docs
+        .map((report) => Report.fromJson(jsonDecode(jsonEncode(report.data()))))
+        .toList();
+    myReports.assignAll(reports);
   }
 }
--- test/test/reaction_test.dart
+++ test/test/reaction_test.dart
@@ -13,56 +13,31 @@ import '../data/repository/test_repository.dart';
 
 void main() {
   group('Test Reaction', () {
-    final fireStore = FakeFirebaseFirestore();
-    late TestRepository testRepository;
+    final controller = MockReportController();
 
     setUp(
       () async {
-        testRepository = TestRepository(firestore: fireStore);
-        final newReport = const Report(
-          serverId: 'testId',
-          workServerId: 'workServerId',
-          createUserId: 'createUserId',
-          createUserName: 'tester',
-          updateDate: '20240103',
-          title: '제목',
-          content: '내용',
-          reactions: [],
-        ).toJson();
-
-        newReport['reactions'] = [
-          const Reaction(emoji: '❤️', reactionUsers: ['test', 'test2'])
-        ].map((e) => e.toJson()).toList();
-        fireStore.collection('report').add(newReport);
+        await controller.onInit();
       },
     );
 
     test('get Report and Reaction Model', () async {
-      final result = await testRepository.fetchCollection('report');
+      final reports = controller.myReports
+          .where((report) => report.serverId == 'testId')
+          .first;
       // 테스터 확인
-      expect(result.docs.first['serverId'], 'testId');
-
-      // 리액션 데이터 확인
-      List<Reaction> reactions = result.docs.first['reactions']
-          .map<Reaction>(
-              (dynamic i) => Reaction.fromJson(i as Map<String, dynamic>))
-          .toList();
-      expect(reactions.first.emoji, '❤️');
+      expect(reports.serverId, 'testId');
+      expect(reports.reactions?.first.emoji, '❤️');
     });
 
     testWidgets('Reaction Widget Test', (WidgetTester tester) async {
-      final result = await testRepository.fetchCollection('report');
-      final getReports = result.docs
-          .map((report) =>
-              Report.fromJson(jsonDecode(jsonEncode(report.data()))))
-          .toList();
+      final reports = controller.myReports;
 
       // Controller를 초기화하고 GetView 위젯을 빌드
       await tester.pumpWidget(GetMaterialApp(
         home: Scaffold(
             // 리액션 위젯 추가
-            body: ReportReactionChip(
-                report: getReports.first, isMyReport: false)),
+            body: ReportReactionChip(report: reports.first, isMyReport: false)),
         initialBinding: BindingsBuilder(() {
           Get.put<ReportController>(MockReportController());
         }),
@@ -70,8 +45,8 @@ void main() {
 
       // 위젯이 화면에 제대로 표시되었는지 확인
       expect(
-          find.text('${getReports.first.reactions?.first.emoji}  '
-              '${getReports.first.reactions?.first.reactionUsers.length}'),
+          find.text('${reports.first.reactions?.first.emoji}  '
+              '${reports.first.reactions?.first.reactionUsers.length}'),
           findsOneWidget);
     });
   });
